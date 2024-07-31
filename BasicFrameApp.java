7321fe9e98a5fd0e22bd9ea304617d07bb9384f2
Ntabayami <syabongambuli7@gmail.com>
2024-07-31 12:48:38 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,15 @@
+
+package basicframeapp;
+
+import za.ac.tut.MyFirstFrame;
+
+public class BasicFrameApp 
+{
+    public static void main(String[] args)
+    {
+        MyFirstFrame myFrame;
+        
+        myFrame = new MyFirstFrame();
+    }
+    
+}
